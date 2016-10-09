063ae54bf5f4d95f9c611b22d68e77618ccaf347
brus <imchengr@gmail.com>
2016-10-09 21:57:55 +0800

    add sample code

--- TechSkill_Refactoring/CH0601ExtraMethod/Program.cs
+++ TechSkill_Refactoring/CH0601ExtraMethod/Program.cs
@@ -15,4 +15,5 @@ namespace CH0601ExtraMethod
     }
 
 
+
 }
---
+++ TechSkill_Refactoring/CH0601ExtraMethod/SampleExtraMethod.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CH0601ExtraMethod
+{
+    public class SampleExtraMethod
+    {
+        private object _name;
+
+        public void PrintOwing(double amount)
+        {
+            printBanner();
+
+            Console.WriteLine("Name:" + _name);
+            Console.WriteLine("amount"+amount.ToString());
+        }
+
+        private void printBanner()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
