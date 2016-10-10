f0e3f0e6b88fb4d34de74b6afbf6b00ffe28bf86
brus <imchengr@gmail.com>
2016-10-11 01:56:56 +0800

    ch0607 add example

---
+++ TechSkill_Refactoring/CH0601ExtraMethod/CH06/CH0607_RemoveAssignmentToParameters.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CH0601ExtraMethod.CH06
+{
+    public class CH0607_RemoveAssignmentToParameters
+    {
+        public int Discount(int inputVal, int quantity, int yearToDate)
+        {
+            if (inputVal>50)
+            {
+                inputVal -= 2;
+            }
+
+            if (quantity>100)
+            {
+                inputVal -= 1;
+            }
+
+            if (yearToDate >10000)
+            {
+                inputVal -= 4;
+            }
+
+            return inputVal;
+        }
+    }
+}
