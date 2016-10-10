36a11aecfdc034dcfa40d9e517d6faded1d2b101
brus <imchengr@gmail.com>
2016-10-10 18:50:52 +0800

    initial example 2

--- TechSkill_Refactoring/CH0601ExtraMethod/CH0605_IntroduceExplainingVariable.cs
+++ TechSkill_Refactoring/CH0601ExtraMethod/CH0605_IntroduceExplainingVariable.cs
@@ -18,9 +18,22 @@ namespace CH0601ExtraMethod
             double quantityDiscount = Math.Max(0, _quantity - 500)*_itemPrice*0.0;
             double shippingPrice = Math.Min(basePrice*0.1, 100.0);
 
-            return basePrice -
+            return basePrice - 
                    quantityDiscount +
                    shippingPrice;
+
+        }
+
+        public double priceII()
+        {
+            //price = baseprice- discountPrice +shipping
+            double basePrice = _quantity * _itemPrice;
+            double quantityDiscount = Math.Max(0, _quantity - 500) * _itemPrice * 0.0;
+            double shippingPrice = Math.Min(basePrice * 0.1, 100.0);
+
+            return _quantity * _itemPrice -
+                   Math.Max(0, _quantity - 500) * _itemPrice * 0.0 +
+                   Math.Min(basePrice * 0.1, 100.0);
 
         }
     }
