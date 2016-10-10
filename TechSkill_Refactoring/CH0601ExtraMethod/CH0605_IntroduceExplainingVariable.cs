7a579829bb853b35ad8982666da4f30ade354bf4
brus <imchengr@gmail.com>
2016-10-10 18:42:24 +0800

    ch 6.5 introduce explaining variable example

---
+++ TechSkill_Refactoring/CH0601ExtraMethod/CH0605_IntroduceExplainingVariable.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CH0601ExtraMethod
+{
+    public class CH0605_IntroduceExplainingVariable
+    {
+        private double _quantity;
+        private double _itemPrice;
+
+        public double price()
+        {
+            return _quantity*_itemPrice -
+                   Math.Max(0, _quantity - 500)*_itemPrice*0.05 +
+                   Math.Min(_quantity*_itemPrice*0.1, 100.0);
+
+        }
+    }
+}
