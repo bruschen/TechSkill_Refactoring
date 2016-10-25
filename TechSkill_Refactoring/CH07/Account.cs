f2c8190afece12a42b6a389df92abb853b9adc10
imchengr <imchengr@gmail.com>
2016-10-25 19:01:29 +0800

    CH07.1 move method

---
+++ TechSkill_Refactoring/CH07/Account.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CH07
+{
+    public class Account
+    {
+        private AccountType _accountType;
+        private int _daysOverDrawn;
+
+        
+
+        public double BankChange()
+        {
+            double result = 4.5;
+
+            if (_daysOverDrawn > 0)
+            {
+                result += this._accountType.OverDraftCharge();
+            }
+
+            return result;
+
+        }
+    }
+}
---
+++ TechSkill_Refactoring/CH07/AccountType.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CH07
+{
+    internal class AccountType
+    {
+        public int daysOverDrawn { get; private set; }
+
+        internal bool IsPremium()
+        {
+            throw new NotImplementedException();
+        }
+
+        public double OverDraftCharge()
+        {
+            if (this.IsPremium())
+            {
+                double result = 10;
+
+                if (daysOverDrawn > 7)
+                {
+                    result += (daysOverDrawn - 7) * 0.85;
+                }
+
+                return result;
+            }
+
+            else
+            {
+                return daysOverDrawn * 1.75;
+            }
+        }
+    }
+}
